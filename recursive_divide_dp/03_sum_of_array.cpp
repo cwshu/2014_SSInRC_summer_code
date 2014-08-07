c5ef639b85233e5ab6b71f43d4e4f6bfb5e5439d
susu <u1240976@gmail.com>
2014-08-08 03:06:21 +0800

    merge to new version

--- recursive_divide_dp/03_sum_of_array.cpp
+++ recursive_divide_dp/03_sum_of_array.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int sum_of_array(int array[], int term){
+void array_copy(int arr1[], int arr2[], int term);
+int array[10];
+
+int sum_of_array(int term){
     // 計算陣列中每個數字的和, term 表示陣列有幾項
     if( term == 1 )
         return array[0];
     else
-        return array[term-1] + sum_of_array(array, term-1);
+        return array[term-1] + sum_of_array(term-1);
 }
 
 int main(){
@@ -17,8 +20,18 @@ int main(){
         {10, -9, 8, -7, 6, -5, 4, -3, 2, -1}, // 5
     };
 
-    cout << sum_of_array(testcase[0], 1) << endl;
-    cout << sum_of_array(testcase[1], 5) << endl;
-    cout << sum_of_array(testcase[2], 4) << endl;
-    cout << sum_of_array(testcase[3], 10) << endl;
+    array_copy(array, testcase[0], 1);
+    cout << sum_of_array(1) << endl;
+    array_copy(array, testcase[1], 5);
+    cout << sum_of_array(5) << endl;
+    array_copy(array, testcase[2], 4);
+    cout << sum_of_array(4) << endl;
+    array_copy(array, testcase[3], 10);
+    cout << sum_of_array(10) << endl;
+}
+
+void array_copy(int arr1[], int arr2[], int term){
+    for(int i=0; i<term; i++){
+        arr1[i] = arr2[i];
+    }
 }
--- recursive_divide_dp/04_sum_of_array_even.cpp
+++ recursive_divide_dp/04_sum_of_array_even.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int sum_of_array_even(int array[], int term){
+void array_copy(int arr1[], int arr2[], int term);
+int array[10];
+
+int sum_of_array_even(int term){
     // 計算陣列中偶數項和, term 表示陣列有幾項, 請使用遞迴手法完成.
     // 偶數項: 2, 4, 6 ... 項為 array[1], array[3], array[5] ... 
 }
@@ -14,8 +17,18 @@ int main(){
         {10, -9, 8, -7, 6, -5, 4, -3, 2, -1}, // -25
     };
 
-    cout << sum_of_array_even(testcase[0], 1) << endl;
-    cout << sum_of_array_even(testcase[1], 5) << endl;
-    cout << sum_of_array_even(testcase[2], 4) << endl;
-    cout << sum_of_array_even(testcase[3], 10) << endl;
+    array_copy(array, testcase[0], 1);
+    cout << sum_of_array_even(1) << endl;
+    array_copy(array, testcase[1], 5);
+    cout << sum_of_array_even(5) << endl;
+    array_copy(array, testcase[2], 4);
+    cout << sum_of_array_even(4) << endl;
+    array_copy(array, testcase[3], 10);
+    cout << sum_of_array_even(10) << endl;
+}
+
+void array_copy(int arr1[], int arr2[], int term){
+    for(int i=0; i<term; i++){
+        arr1[i] = arr2[i];
+    }
 }
